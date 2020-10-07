43caec16db747a5676a22d3475a4749076a39984
4TfCl5P7X7ce <roshanstark007@gmail.com>
2020-10-07 20:40:04 +0530

    Fix typo's

--- startWebApp.sh
+++ startWebApp.sh
@@ -1,3 +1,3 @@
-cd ~/PriceTracker/client/ && npm install && npm run build
+cd ~/PriceTracker/client/ && npm install && pm2 start --name client npm -- start
 
 cd ~/PriceTracker/server/ && npm install && pm2 start server.js
